79b4cd51f4c63f7bec217b9f5485cb30db226b06
Jonathan Wight <jwight@mac.com>
2011-11-01 10:43:30 -0700

    Updated licenses & README

--- NSObject_KVOBlock.m
+++ NSObject_KVOBlock.m
@@ -1,10 +1,33 @@
 //
 //  NSObject_KVOBlock.m
-//  MOO
+//  TouchCode
 //
 //  Created by Jonathan Wight on 07/24/11.
 //  Copyright 2011 toxicsoftware.com. All rights reserved.
 //
+//  Redistribution and use in source and binary forms, with or without modification, are
+//  permitted provided that the following conditions are met:
+//
+//     1. Redistributions of source code must retain the above copyright notice, this list of
+//        conditions and the following disclaimer.
+//
+//     2. Redistributions in binary form must reproduce the above copyright notice, this list
+//        of conditions and the following disclaimer in the documentation and/or other materials
+//        provided with the distribution.
+//
+//  THIS SOFTWARE IS PROVIDED BY TOXICSOFTWARE.COM ``AS IS'' AND ANY EXPRESS OR IMPLIED
+//  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+//  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL TOXICSOFTWARE.COM OR
+//  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+//  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+//  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+//  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+//  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//  The views and conclusions contained in the software and documentation are those of the
+//  authors and should not be interpreted as representing official policies, either expressed
+//  or implied, of toxicsoftware.com.
 
 #import "NSObject_KVOBlock.h"
 
@@ -44,7 +67,7 @@ static void *KVO;
         }
 
     CKVOBlockHelper *theHelper = [CKVOBlockHelper helperForObject:self create:YES];
-    
+
     id theKey = [theHelper keyForKeyPath:inKeyPath identifier:inIdentifier];
 
     if ([theHelper.blocksForIdentifier objectForKey:theKey] != NULL)
@@ -52,25 +75,25 @@ static void *KVO;
         void *theContext = (__bridge void *)[theHelper canonicalKeyForKey:theKey];
         [self removeObserver:theHelper forKeyPath:inKeyPath context:theContext];
         }
-    
+
     [theHelper.blocksForIdentifier setObject:[inHandler copy] forKey:theKey];
-    
+
     void *theContext = (__bridge void *)[theHelper canonicalKeyForKey:theKey];
     [self addObserver:theHelper forKeyPath:inKeyPath options:inOptions context:theContext];
-    
+
     return(theKey);
     }
 
 - (void)removeKVOBlockForToken:(id)inToken
     {
     CKVOBlockHelper *theHelper = [CKVOBlockHelper helperForObject:self create:NO];
-    
+
     if (theHelper == NULL)
         {
         NSLog(@"KVOBlock never registered?");
         return;
         }
-    
+
     NSAssert(theHelper != NULL, @"KVOBlock never registered");
 
     void *theContext = (__bridge void *)[theHelper canonicalKeyForKey:inToken];
@@ -124,7 +147,7 @@ static void *KVO;
     if (theHelper == NULL && inCreate == YES)
         {
         theHelper = [[CKVOBlockHelper alloc] initWithObject:self];
-        
+
         objc_setAssociatedObject(inObject, &KVO, theHelper, OBJC_ASSOCIATION_RETAIN);
         }
     return(theHelper);
@@ -157,7 +180,7 @@ static void *KVO;
     {
     return([NSArray arrayWithObjects:inKeyPath, inIdentifier, NULL]);
     }
-    
+
 - (NSString *)keypathForKey:(id)inKey
     {
     return([inKey objectAtIndex:0]);
@@ -175,9 +198,9 @@ static void *KVO;
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context;
     {
     NSParameterAssert(context);
-    
+
     NSArray *theKey = (__bridge NSArray *)context;
-    
+
     KVOBlock theBlock = [self.blocksForIdentifier objectForKey:theKey];
     if (theBlock == NULL)
         {
